cb5d003287614d749283c4638cbbe29602877c1b
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-16 10:47:57 +0000

    solution

--- main.py
+++ main.py
@@ -15,7 +15,12 @@ travel_log = [
 #TODO: Write the function that will allow new countries
 #to be added to the travel_log. 👇
 
-
+def add_new_country(country_visited, times_visited, cities_visited):
+  new_country = {}
+  new_country["country"] = country_visited
+  new_country["visits"] = times_visited
+  new_country["cities"] = cities_visited
+  travel_log.append(new_country)
 
 #🚨 Do not change the code below
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
